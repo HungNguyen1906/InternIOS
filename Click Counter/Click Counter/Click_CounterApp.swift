0abb49dadf386eedb173e5d3051157e3d0377a34
HungNguyen1906 <111716256+HungNguyen1906@users.noreply.github.com>
2022-08-26 17:16:59 +0700

    Add files via upload

---
+++ Click Counter/Click Counter/Click_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Click_CounterApp.swift
+//  Click Counter
+//
+//  Created by MaxMobile on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Click_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
